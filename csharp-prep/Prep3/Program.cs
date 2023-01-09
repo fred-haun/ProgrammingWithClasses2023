58d7cac838a24ec114b9b3f896315a4907e343e3
Freddy Haun <hau18002@byui.edu>
2023-01-09 09:57:27 -0700

    in class work 1/9/23

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        //Console.WriteLine($"Your random number is {number}!");
+        bool guessed = false;
+        int turns = 1;
+        while(guessed == false)
+        {
+            Console.Write("Please enter your guess between 1 and 100: ");
+            int guess = int.Parse(Console.ReadLine());
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+                turns += 1;
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+                turns += 1;
+            }
+            else
+            {
+                Console.WriteLine("You got it Bruh!");
+                guessed = true;
+            }
+        }
+        Console.WriteLine($"It took you {turns} turns to guess correctly.");
+        //Console.WriteLine($"They guessed {guess}");
+
     }
 }
