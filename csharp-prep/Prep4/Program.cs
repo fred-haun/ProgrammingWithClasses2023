a35f021004b2c31023a701745e0dc036b1e33e4b
fred-haun <hau18002@byui.edu>
2023-01-11 11:20:15 -0700

    prep4 done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<float> numbers = new List<float>();
+        bool isZero = false;
+        while(isZero == false)
+        {
+            Console.Write("Enter number: ");
+            int enterNumber = int.Parse(Console.ReadLine());
+            if (enterNumber == 0)
+            {
+                isZero = true;
+            }
+            else
+            {
+                numbers.Add(enterNumber);
+            }
+
+        }
+        int sum = 0;
+        float totalNum = 0;
+        int biggestNum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+            totalNum += 1;
+            if (number > biggestNum)
+            {
+                biggestNum = number;
+            }
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {sum/totalNum}");
+        Console.WriteLine($"The largest number is: {biggestNum}");
+        //Console.WriteLine("Hello Prep4 World!");
     }
 }
