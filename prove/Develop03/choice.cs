5d2c7bf2550374bf772dce9df59fd138f57f495d
Freddy Haun <hau18002@byui.edu>
2023-02-08 10:03:51 -0700

    classwork 2/8/23

--- prove/Develop03/choice.cs
+++ prove/Develop03/choice.cs
@@ -9,6 +9,11 @@ public class choice
         scripture.scripture();
         Console.WriteLine("");
         Console.WriteLine("Press enter to continue or type 'quit' to finish");
-        string balls = Console.ReadLine();
+        string Choice = Console.ReadLine();
+        string choice = Choice.ToLower();
+        if(choice == "quit")
+        {
+            Console.WriteLine("Goodbye!");
+        }
     }
 }
--- prove/Develop03/getscripture.cs
+++ prove/Develop03/getscripture.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 public class getScripture
 {
@@ -8,7 +9,7 @@ public class getScripture
     {
         Random rand = new Random();
         number = rand.Next(0, 31103);       
-        using(var reader = new StreamReader(@"C:\BYUI_Winter_2023\Programming With Classes\ProgrammingWithClasses2023\prove\Develop03\kjv.csv"))
+        using(var reader = new StreamReader(@"C:\Users\fredr\Desktop\Winter 2023\Programming With Classes\ProgrammingWithClasses2023\prove\Develop03\kjv.csv"))
         {
             List<string> reference = new List<string>();
             List<string> book = new List<string>();
@@ -28,7 +29,15 @@ public class getScripture
                 verseNumber.Add(values[4]);
                 verse.Add(values[5]);
             }
-            Console.WriteLine($"{book[number]} {chapter[number]}:{verseNumber[number]}\n{verse[number]}");
+            string[] verseWord = verse[number].Split(' ');
+            foreach(var word in verseWord)
+            {
+                System.Console.Write($"{word} ");
+            }
+            Console.WriteLine("");
+            Console.WriteLine($"{book[number]} {chapter[number]}:{verseNumber[number]}\n");
+            int wordCount = verseWord.Count(); 
+            Console.WriteLine(wordCount);
         }
     }
 }
